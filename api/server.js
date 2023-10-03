ca5369deb34b3fcce90ed5e292c31f548bdc9b4e
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-10-03 10:51:59 -0500

    Build server in server.js and require it in index.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require("express")
+
+const projectsRouter = require("../api/project/router")
+const resourcesRouter = require("../api/resource/router")
+const tasksRouter = require("../api/task/router")
+
+const server = express()
+
+server.use(express.json())
+
+server.use("/api/projects", projectsRouter)
+server.use("/api/resources", resourcesRouter)
+server.use("/api/tasks", tasksRouter)
+
+server.use("*", (req, res)=>{
+    res.json({ api : "fallback" })
+})
+
+module.exports = server 
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require("dotenv").config()
+
+const server = require("./api/server.js")
+
+const port = process.env.PORT 
+
+server.listen( port, () => console.log(`n/API running on port ${port}\n`))
